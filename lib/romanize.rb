4dcba5c65c6e21a6c1180ce205e795ee3c60b83e
itgsod-oscar-johansson <oscar.johansson@elev.it-gymnasiet.se>
2014-06-01 12:04:55 +0200

    Clear test with triple digits

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -4,7 +4,7 @@ def romanize(number)
   raise ArgumentError,'can not encode negative number' if number < 0
 
 
-  library = {1 => 'I', 4 => 'IV', 5 => 'V', 9 => 'IX', 10 => 'X', 40 => 'XL', 50 => 'L', 90 => 'XC'}
+  library = {1 => 'I', 4 => 'IV', 5 => 'V', 9 => 'IX', 10 => 'X', 40 => 'XL', 50 => 'L', 90 => 'XC', 100 => 'C', 400 => 'CD', 500 => 'D', 900 =>'CM'}
   new_number = ''
 
   library.keys.sort.reverse.each do |key|
