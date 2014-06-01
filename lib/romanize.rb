483f91cdf53bddf13251cad0d563cb1b735fc506
itgsod-oscar-johansson <oscar.johansson@elev.it-gymnasiet.se>
2014-06-01 12:01:03 +0200

    Clear test with single digit
    Also removed the return string test.

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -3,6 +3,16 @@ def romanize(number)
   raise ArgumentError,'can not encode zero' if number.zero?
   raise ArgumentError,'can not encode negative number' if number < 0
 
-  return('kött')
 
+  library = {1 => 'I', 4 => 'IV', 5 => 'V', 9 => 'IX'}
+  new_number = ''
+
+  library.keys.sort.reverse.each do |key|
+      while number >= key
+        new_number += library[key]
+        
+        number -= key
+      end
+  end
+  return new_number
 end
